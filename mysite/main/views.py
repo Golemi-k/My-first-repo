8eae5c83abf73f7e178975f6466629dc7ee1a099
Golemi-k <jan.filipowicz777@gmail.com>
2025-07-23 16:09:22 +0200

    Created new django webapp(form tag searching) 🤑

--- mysite/main/views.py
+++ mysite/main/views.py
@@ -1,22 +1,53 @@
 from django.shortcuts import render
-import random
+from urllib.request import urlopen
+from urllib.error import URLError, HTTPError
+from bs4 import BeautifulSoup
 
-def index(request):
-    message = None
+def home(request):
+    return render(request, "main/home.html")
+
+def about(request):
+    return render(request, "main/about.html")
+
+def contact(request):
+    return render(request, "main/contact.html")
+
+def scraping(request):
+    context = {
+        'url': '',
+        'html_scrape': None,
+        'form_count': 0,
+        'form_id':[]
+    }
 
     if request.method == 'POST':
-        user_name = request.POST.get('user_name')
-        guess = request.POST.get('guess')
+        url = request.POST.get('url')
+        try:
+            page = urlopen(url)
+            html_bytes = page.read()
+            html_scrape = html_bytes.decode("utf-8")
+
+            soup = BeautifulSoup(html_scrape, 'html.parser')
+            forms = soup.find_all('form')
+
+            form_id = []
+            for form in forms:
+                if form.get('id'):
+                    form_id.append(f"id: {form.get('id')}")
+                elif form.get('action'):
+                    form_id.append(f"action: {form.get('action')}")
+                else:
+                    form_id.append("brak id i action")
+                
+
+            context['url'] = url
+            context['html_scrape'] = html_scrape
+            context['form_count'] = len(forms)
+            context['form_id'] = form_id
 
 
-        guess = int(guess)
-        random_number = random.randint(1, 10)
+        except (HTTPError, URLError) as e:
+            context['html_scrape'] = f"Błąd: {e}"
 
-        if guess == random_number:
-            message = f"Brawo {user_name}! Trafiłeś liczbę {random_number}!"
-        else:
-            message = f"Niestety {user_name}, nie trafiłeś. Wylosowana liczba to {random_number}."
+    return render(request, "main/scraping.html", context)
 
-
-
-    return render(request, 'hello.html', {'message': message})
